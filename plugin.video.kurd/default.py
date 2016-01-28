916f81e6d01fdf987e6ae5bcb3de44db3592bd8c
Halow <halow.karim@gmail.com>
2016-01-28 22:07:27 +0100

    fix kurdsat

--- plugin.video.kurd/default.py
+++ plugin.video.kurd/default.py
@@ -30,7 +30,7 @@ def iptvxtra_play():
     sys.exit(0)
 
 def main():
-    add_video_item('http://wpc.C1A9.edgecastcdn.net/hls-live/20C1A9/kurdsat/ls_satlink/b_528.m3u8'				,{ 'title': 'KurdSat TV'}, icons + 'http://karwan.tv/images/tvlogo/kurdsat-tv.png')
+    add_video_item('http://198.100.158.231:1935/kanal11/_definst_/livestream/playlist.m3u8'				,{ 'title': 'KurdSat TV'}, icons + 'http://karwan.tv/images/tvlogo/kurdsat-tv.png')
     add_video_item('rtmp://68.168.105.117/live//livestream'	,{ 'title': 'KurdSat News'}, icons + 'http://karwan.tv/images/tvlogo/kurdsat-news-tv.png')
     add_video_item('rtmp://84.244.187.12/live/livestream'			,{ 'title': 'Kurdistan TV'}, icons + 'http://karwan.tv/images/tvlogo/kurdistan-tv.png')
     add_video_item('http://198.100.158.231:1935/kanal10/_definst_/livestream/playlist.m3u8'				,{ 'title': 'Zagros TV'}, icons + 'http://karwan.tv/images/tvlogo/zagros-tv.png')
