84ef83a7c9e258cf209540c2fdd913e9324a6f67
HalowTV <halow.karim@gmail.com>
2016-12-14 19:39:32 +0100

    Update and add new Movies add-ons

---
+++ plugin.video.iranproud/default.py
@@ -0,0 +1,258 @@
+import xbmcaddon,os,requests,xbmc,xbmcgui,urllib,urllib2,re,xbmcplugin
+from BeautifulSoup import BeautifulSoup
+
+movlink = "http://www.iranproud.net/irani-best-movies"
+
+
+
+
+def menu1():
+    r = requests.get('http://www.iranproud.net/index.html')
+    urlshit = ''
+    #regex = r'<div id=".*?" class="fluid"><a href=".+?80(.*?)">.+?<div id="divTitrSS2" class="TitrSS">(.*?)</div></a>'
+    regex = 'class="fluidButton"><a href.+?"(.*?)".+?(.*?)</a></div>'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for link,name in match:
+        xbmc.log("LINK HERE --->>> %s "% link)
+        addDir3(name.replace('target="_parent">',''),link.replace('http://www.iranproud.net:80','').replace('/','http://www.iranproud.net/'),3,'','','')
+
+def lifetvmenu(url):
+    r = requests.get(url)
+    #regex = r'<div id="divTitrGrid2" class="titrGrid2">(.*?)</div>.+?<ul id="gridMason" class="gridMason effect-1">.+?<li><a href=".+?" target="_parent"><img src=".+?" alt="" width="100" height="160"></a></li>.+?</ul>'
+    regex = r'<div id="divTitrGrid2" class="titrGrid2">(.*?)</div>'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for name2 in match:
+        addDir3(name2.replace('&amp;','&'),'http://www.iranproud.net/livetv',11,'','','')
+
+def recrel(url):
+    r = requests.get(url)
+    regex = r'<div id="divTitrSS1" class="TitrSS">(.*?)</div>'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for name2 in match:
+        if "musicvideo" in str(url):
+            addDir3(name2.replace('&amp;','&'),'http://www.iranproud.net/musicvideo',9,'','','')
+def menu2(url):
+    r = requests.get(url)
+    regex = r'<div id="divTitrSS2" class="TitrSS">(.*?)</div>.+?<div id="divSutitrSS2" class="sutitrSS"><a href=".+?80(.*?)">VIEW ALL</a></div>'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for name2,link in match:
+        if "musicvideo" in str(url):
+            addDir3(name2.replace('&amp;','&'),"http://www.iranproud.net%s"%link.replace('&amp;','&'),9,'','','')
+        elif "iran-best-movies" in str(url):
+            addDir3(name2.replace('&amp;','&'),"http://www.iranproud.net%s"%link.replace('&amp;','&'),10,'','','')
+        else:
+            addDir3(name2.replace('&amp;','&'),"http://www.iranproud.net%s"%link.replace('&amp;','&'),4,'','','')
+def tsc(url):
+    r = requests.get(url)
+    regex = r'<div class="divBorder.*?"><a href=".+?80(.*?)"><img src="(.*?)" alt=""></a></div>.+?">.+?<div class="SSh.*?">(.*?)</div>.+?<'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for link,image,name3 in match:
+        xbmc.log("3rd menu links --->>> %s "% match)
+        addDir3(name3.replace('&amp;','&'),"http://www.iranproud.net%s"%link.replace('" target="_parent','').replace('&amp;','&'),5,image,'','')
+   
+def musicvideo(url):
+    r = requests.get(url)
+     #<div class="divBorder?"><a href=".+?80(.*?)"><img src="(.*?)" alt=""></a></div>.+?">.+?<div class="SSh?">(.*?)</div>.+?<
+    regex = r'<div class="divBorder"><a href="(.*?)" target="_parent"><img src="(.*?)" alt=""></a></div>.+?<div class="mask1M">.+?<div class="SSh1">(.*?)</div>'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for link,image,name3 in match:
+        xbmc.log("3rd menu links --->>> %s "% match)
+        addDir2(name3.replace('&amp;','&'),"http://www.iranproud.net%s"%link.replace('" target="_parent','').replace('&amp;','&'),6,image)
+
+def musicvideo2(url):
+    r = requests.get(url)
+    regex = r'<div class="divPicMask1 divPicMask1_musicvideos"><a href="#" target="_blank"><img src="(.*?)" alt="" width="210" height="160"></a>.+?<div class="SSh1">(.*?)</div>.+?<a href="(.*?)" target="_parent" class="infinA1">Watch Now</a>'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for image,name3,link in match:
+        xbmc.log("3rd menu links --->>> %s "% match)
+        addDir2(name3.replace('&amp;','&'),"http://www.iranproud.net%s"%link.replace('" target="_parent','').replace('&amp;','&'),6,image)
+
+
+def livetv(url):
+   r = requests.get(url)
+   regex = r'<li><a href="(.*?)" target="_parent"><img src="(.*?)" alt="" width="100" height="160"></a></li>'
+   match = re.compile(regex,re.DOTALL).findall(r.content)
+   for link,image in match:
+    xbmc.log('LIVETV NOTICE:%s'%match)
+    addDir2('test',"http://www.iranproud.net%s"%link,6,image) 
+
+#needs fixing nigguh
+def movies(url):
+    #html = '<html><div class="divBorder3"><a href="(.*?)" target="_parent"><img src="(.*?)" alt="" width="172" height="240"></a></div><div class="mask3M"><div class="SSh3">(.*?)</div>'
+    html = BeautifulSoup(requests.get(url).text) # get the html
+    grid = html.findAll("ul", attrs={'class': 'gridMasonMO'})[0] #find the ul with id gridMason4
+    items = grid.findAll("li") # find all li items in the ul
+    for item in items:  # for each item
+        divborder = item.findAll("div", attrs={'class': 'divBorder3'})[0] #find the first div with class divborder 3
+        href = divborder.findAll('a')[0]['href'] # find the link href in the div
+        image = divborder.findAll('img')[0]['src'] #find the image src in the div
+        ssh3 = item.findAll('div', attrs={'class': 'SSh3'})[0].text #get the text of the first div with class SSh3 in the li item
+        xbmc.log('item:' + repr(item))
+        addDir2(ssh3.replace('&amp;','&'),"http://www.iranproud.net%s"%href.replace('" target="_parent','').replace('&amp;','&'),12,image)
+ 
+def menu4(url):
+    r = requests.get(url)
+    regex = r'class="divBorder.*?"><a href="(.*?)".*?<img src="(.*?)" alt=.+?<div id="divEpiNo1" class="fluid">(.*?)</div>'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for link,image,name4 in match:
+        xbmc.log("4th menu links --->>> %s "% match)
+        addDir2(name4.replace('&amp;','&'),"http://www.iranproud.net%s"%link.replace('" target="_parent','').replace('&amp;','&'),6,image)
+
+def menu5(url):
+    r = requests.get(url)
+    regex = r'videosrc="(.*?)"'
+    match = re.compile(regex).findall(r.content)
+    for link in match:
+        xbmc.log("link play this %s"%link)
+        playVideo(link)
+
+def menu6(url):
+    r = requests.get(url)
+    regex = r'video_url="(.*?)";'
+    match = re.compile(regex).findall(r.content)
+    for link in match:
+        xbmc.log("link play this %s"%link)
+        playVideo(link)    
+
+###----Playing the stupid video---##
+def playVideo(url):
+    player = xbmc.Player()
+    player.play(url)
+
+
+#--------------Params---------#
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]
+                                
+        return param       
+#################################################################################################################
+
+#                               NEED BELOW CHANGED
+
+
+
+def addDir(name,url,mode,iconimage):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+     
+def addDir2(name,url,mode,iconimage):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        liz.setProperty('IsPlayable','true')
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
+###############################################################################################################        
+
+def addDir3(name,url,mode,iconimage,fanart,description):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&fanart="+urllib.quote_plus(fanart)+"&description="+urllib.quote_plus(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description } )
+        liz.setProperty( "Fanart_Image", fanart )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+
+def addLink(name,url,image,urltype,fanart):
+  ok=True
+  liz=xbmcgui.ListItem(name, iconImage=image, thumbnailImage=image)
+  liz.setInfo( type="Video", infoLabels={ "Title": name } )
+  liz.setProperty('IsPlayable','true')
+  liz.setProperty('fanart_image', fanart)
+  ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+  return ok
+
+def setView(content, viewType):
+    # set content type so library shows more views and info
+    if content:
+        xbmcplugin.setContent(int(sys.argv[1]), content)
+    if ADDON.getSetting('auto-view')=='true':
+        xbmc.executebuiltin("Container.SetViewMode(%s)" % viewType )
+ 
+
+
+              
+params=get_params()
+url=None
+name=None
+mode=None
+iconimage=None
+fanart=None
+description=None
+
+
+try:
+        url=urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name=urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        iconimage=urllib.unquote_plus(params["iconimage"])
+except:
+        pass
+try:        
+        mode=int(params["mode"])
+except:
+        pass
+try:        
+        fanart=urllib.unquote_plus(params["fanart"])
+except:
+        pass
+try:        
+        description=urllib.unquote_plus(params["description"])
+except:
+        pass
+   
+print "Mode: "+str(mode)
+print "URL: "+str(url)
+print "Name: "+str(name)
+
+if mode==None or url==None or len(url)<1:
+        print ""
+        menu1()
+       
+elif mode==3:
+        menu2(url)
+        recrel(url)
+        lifetvmenu(url)
+elif mode==4:
+        tsc(url)
+elif mode==5:
+        menu4(url)
+elif mode==6:
+        menu5(url)
+elif mode==7:        
+        playVideo(url)
+elif mode==8:
+        checkmenu(url)
+elif mode==9:
+        musicvideo(url)
+        musicvideo2(url)
+elif mode==10:
+        movies(url)
+elif mode==11:
+        livetv(url)
+elif mode==12:
+		menu6(url)
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
---
+++ plugin.video.kurdsubtitle/default.py
@@ -0,0 +1,311 @@
+import xbmcaddon, xbmc, xbmcgui, xbmcplugin
+import requests, urllib, urllib2, re, urlparse
+from os import path, system
+from urllib2 import Request, URLError, urlopen
+import sys, json, os
+import unicodedata
+import googledocs as gDocs
+import commonresolvers as resolve
+import urlresolver
+from BeautifulSoup import BeautifulSoup
+
+
+thisPlugin = int(sys.argv[1])
+addonId = "plugin.video.cinama"
+dataPath = xbmc.translatePath('special://profile/addon_data/%s' % (addonId))
+addon = xbmcaddon.Addon()
+addonInfo = xbmcaddon.Addon().getAddonInfo
+progress = xbmcgui.DialogProgress()
+path = addon.getAddonInfo('path')
+
+
+###----Playing the stupid video---##
+def playVideo(url):
+    resolved = urlresolver.resolve(url)
+    xbmc.log("resolved: " + repr(resolved))
+    if resolved:
+        url = resolved
+    player = xbmc.Player()
+    player.play(url)
+
+
+
+# for get content for website
+def getUrl(url, post='', referer=''):
+    if post: req = urllib2.Request(url, post)
+    else: req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    if referer:
+      req.add_header('Referer', referer)
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    return link
+
+
+
+
+
+
+
+baseurl = 'http://www.kurdsubtitle.net/'
+#Main menu#
+def home():
+    addDir3("Genres", "foo", 5, "", "","")
+    addDir3("Years", "foo", 6, "", "","")
+
+def categories():
+    r = requests.get(baseurl)
+    html = BeautifulSoup(r.content)
+    genres_ul = html.findAll("ul", attrs={'class':'genres scrolling'})[0]
+    genres_lis = genres_ul.findAll('li')
+    for genre in genres_lis:
+        link = genre.findAll("a")[0]
+        href = link["href"]
+        name = link.text
+        name = name.encode("utf-8")
+        number_of_items = genre.findAll("i")[-1].text
+        name = "({0}) {1}".format(number_of_items, name)
+        if "tvshows" in str(url):
+            addDir3(name,href,7,'','','')
+        else:
+            addDir3(name,href,2,'','','')
+
+def years():
+    r = requests.get(baseurl)
+    html = BeautifulSoup(r.content)
+    years_ul = html.findAll("ul", attrs={'class': 'year scrolling'})[0]
+    years_lis = years_ul.findAll('li')
+    for year in years_lis:
+        link = year.findAll("a")[0]
+        href = link["href"]
+        name = link.text
+        name = name.encode("utf-8")
+        if "tvshows" in str(url):
+            addDir3(name,href,7,'','','')
+        else:
+            addDir3(name, href, 2, '', '', '')
+
+#Fix this nigguh <-- start here
+def menu2(url):
+    r = requests.get(url)
+    html = BeautifulSoup(r.content)
+    items_div = html.findAll("div", attrs={"class": "items"})[0]
+    movies = items_div.findAll("article")
+    for movie in movies:
+        poster = movie.findAll("div", attrs={"class": "poster"})[0]
+        link = poster.findAll("a")[0]
+        href = link["href"]
+        image = link.findAll("img")[0]
+        image_src = image["src"].split("?resize")[0]
+        xbmc.log("img_src: " + repr(image_src))
+        name = image["alt"].encode("utf-8")
+        texto = movie.findAll("div", attrs={"class": "texto"})[0].contents[0].encode("utf-8")
+        if "/tvshows" in href:
+            addDir3(name, href + '?tab=episodes',7,image_src,'',texto)
+        else:
+            addDir2(name, href + '?tab=video&player=option-1',  4, image_src,texto)
+    try:
+        pagination = html.findAll("div", attrs={"class": "pagination"})[0]
+        yearstext = pagination.findAll("span")[0].text
+        pages_info = re.findall("Page (\d+) of (\d+).*", yearstext)[0]
+        current_page = int(pages_info[0])
+        max_page = int(pages_info[1])
+        if current_page < max_page:
+            if "/page" in url:
+                url = url.split("/page")[0]
+            href = url + "/page/%s" % str(current_page + 1)
+            addDir3("Next Page", href, 2, '','','')
+    except:
+        pass
+
+
+def tvseastit(url):
+    r = requests.get(url)
+    html = BeautifulSoup(r.content)
+    seasons = html.findAll("div",attrs={"class":"se-c"})
+    for season in seasons:
+        seastitle = season.findAll("span",attrs={"class":"title"})[0].contents[0]
+        addDir3(seastitle,url,8,'','','')
+def tvep(url, season_name):
+    r = requests.get(url)
+    html = BeautifulSoup(r.content)
+    seasons = html.findAll("div", attrs={"class": "se-c"})
+    for season in seasons:
+        seastitle = season.findAll("span",attrs={"class":"title"})[0].contents[0]
+        if seastitle != season_name:
+            continue
+        episodes = season.findAll("ul", attrs={"class":"episodios"})
+        episodes_list = episodes[0].findAll('li')
+        for episode in episodes_list:
+            eplink = episode.findAll("div",attrs={"class":"imagen"})[0]
+            link = eplink.findAll("a")[0]
+            href = link['href']
+            image = link.findAll("img")[0]
+            image_src = image['src']
+            ep_number = episode.findAll("div",attrs=({"class":"numerando"}))[0].text.encode('utf-8')
+            ep_title_div = episode.findAll("div",attrs=({"class":"episodiotitle"}))[0]
+            ep_title = ep_title_div.findAll("a")[0].text.encode("utf-8")
+            ep_date = ep_title_div.findAll("span", attrs={"class": "date"})[0].text
+            name = "{0} - {1} ({2})".format(ep_number, ep_title, ep_date)
+            addDir2(name,href,4,image_src,'')
+def menu3(url):
+    r = requests.get(url)
+    regex = r'<iframe src="(.*?)"'
+    match = re.compile(regex,re.DOTALL).findall(r.content)
+    for link2 in match:
+        addDir2('',link2,4,'')
+def getResolve(url):
+    regex = '\/\/(.+?)\/'
+    match = re.compile(regex).findall(url)[0]
+
+    regex = '(?:www.|)(.+?)\.'
+
+    
+
+    site = re.compile(regex).findall(match)[0]
+    if site == 'drive': site = 'googledocs'
+    urls = __import__('%s' % site, globals(), locals(), ['resolve'], -1)
+    link = urls.resolve(url)
+    return link
+
+
+
+
+#-------------------test-------------------#
+#####---GET STREAM URL and play that mofo---####
+
+def getStream(url):
+    content = getUrl(url)
+    regex = '<iframe src="(.*?)"'
+    link = re.compile(regex, re.DOTALL).findall(content)[0]
+    xbmc.log("LIIIIINK %s "% link)
+    try:
+        #data = getResolve(link)
+        
+        #try:
+        #    vLink = data[0]['url']
+        #except:
+        #    vLink = urls
+        #playVideo(vLink)
+        playVideo(link)
+    except:
+        pass
+
+
+
+
+#----------------------------------Param BS----------------------#
+
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]
+                                
+        return param       
+#################################################################################################################
+
+#                               NEED BELOW CHANGED
+
+def addDir(name,url,mode,iconimage):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+     
+def addDir2(name,url,mode,iconimage,description):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+urllib.quote_plus(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description  } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
+###############################################################################################################        
+
+def addDir3(name,url,mode,iconimage,fanart,description):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&fanart="+urllib.quote_plus(fanart)+"&description="+urllib.quote_plus(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description } )
+        liz.setProperty( "Fanart_Image", fanart )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+
+
+def setView():
+        xbmc.executebuiltin("Container.SetViewMode(500)")
+ 
+
+
+              
+params=get_params()
+url=None
+name=None
+mode=None
+iconimage=None
+fanart=None
+description=None
+
+
+try:
+        url=urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name=urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        iconimage=urllib.unquote_plus(params["iconimage"])
+except:
+        pass
+try:        
+        mode=int(params["mode"])
+except:
+        pass
+try:        
+        fanart=urllib.unquote_plus(params["fanart"])
+except:
+        pass
+try:        
+        description=urllib.unquote_plus(params["description"])
+except:
+        pass
+   
+xbmc.log("Mode: "+str(mode))
+xbmc.log("URL: "+str(url))
+xbmc.log("Name: "+str(name))
+
+if mode==None or url==None or len(url)<1:
+        print ""
+        home()
+elif mode==1:
+        Open_URL()
+elif mode==2:
+        menu2(url)
+elif mode==3:
+        menu3(url)
+elif mode==4:
+        getStream(url)
+elif mode==5:
+        categories()
+elif mode==6:
+        years()
+elif mode==7:
+        tvseastit(url)
+elif mode==8:
+        tvep(url, name)
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
